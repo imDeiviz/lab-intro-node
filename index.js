fabe88e0f28a92804023d333a758d2927a02bc8e
David Rojas <dmrojassantana@gmail.com>
2024-11-09 11:16:31 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+  }
+  return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+  let total = 0;
 
-  avg() {}
+  for (let i = 0; i < this.length; i++) {
+    total += this.items[i];
+  }
+  return total;
+  }
+
+  avg() {
+    if (this.length === 0) { 
+      throw new Error("EmptySortedList"); 
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
